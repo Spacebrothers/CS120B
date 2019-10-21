fbc55790424efb48356139cd03e2fdc159e02e09
An Pho <apho001@wch136-34.cs.ucr.edu>
2019-10-21 12:24:46 -0700

    part6

--- Lab6/main2.c
+++ Lab6/main2.c
@@ -1,9 +1,17 @@
+/*	Author: vtruo009
+ *  Partner(s) Name: An Pho
+ *	Lab Section:
+ *	Assignment: Lab #6  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include <avr/interrupt.h>
 #endif
-
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
@@ -14,7 +22,7 @@ void TimerOn() {
 	TIMSK1 = 0x02;
 	TCNT1 = 0;
 	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
+	SREG != 0x80;
 }
 void TimerOff() {
 	TCCR1B = 0x00;
@@ -27,7 +35,7 @@ void TimerISR() {
 ISR(TIMER1_COMPA_vect) {
 	_avr_timer_cntcurr--;
 	if(_avr_timer_cntcurr == 0) {
-		TimerISR();
+		TIMERISR();
 		_avr_timer_cntcurr = _avr_timer_M;
 	}
 }
@@ -37,10 +45,9 @@ void TimerSet(unsigned long M) {
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-enum states {initial, on1, on2, on3}
 int main(void) {
 
-	DDRB = 0xFF; PORTB = 0x00;
+	DDRB = 0x00; PORTB = 0xFF;
 	TimerSet(1000);
 	TimerOn();
 	unsigned char tmpB = 0x00;
