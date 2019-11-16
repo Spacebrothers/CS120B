1f489562af667e1edba412cf3b44c70dabbbd051
An Pho <apho001@wch136-03.cs.ucr.edu>
2019-11-15 21:50:02 -0800

    No part 3

--- source/main3.c
+++ source/main3.c
@@ -14,172 +14,92 @@
 #include <avr/interrupt.h>
 #endif
 
-unsigned char B = 0x00;
-unsigned char B3 = 0x00;
-unsigned char tasksNum = 2;
-const unsigned long taskPeriodGCD = 100;
-const unsigned long periodbl = 1000;
-const unsigned long periodtl = 300;
+unsigned char c;
 
-//Tasks
-typedef struct task {
-	int state;
-	unsigned long period;
-	unsigned long elapsedTime;
-	int (*TickFnct)(int);
-} task;
+//Tick1
+enum KeyPadStates {keyPadOutput} KeyPadState;
+int KeyPadTick(int state);
 
-//2 state machines
-task tasks[2];
+enum LCDStates {OUTPUT} LCDState;
+int LCDTick(int state);
 
-//FOR TIMER
-volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
-
-void TimerOn() {
-	//AVR timer/counter controller register TCCR1
-	TCCR1B = 0x0B; //bit3 = 0: CTC mode (clear timer on compare)
-	//bit2...bit0 = 11: prescaler /64
-	//00001011: 0x0B
-	//So 8MHz clock or 8,000,000 /64 = 125,000 ticks/s
-
-	//AVR putput compare register OCR1A
-	OCR1A = 125; //timer interrupt will be generated when TCNT == OCR1A
-	TIMSK1 = 0x02; //bit1: OCIE1A -- enables compare match interrupt
+int main(void)
+{
+    DDRA = 0xFF; PORTA = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
 	
-	//initiate AVR counter
-	TCNT1 = 0;
+	unsigned long systemPeriod = 250;
+	static task task1;
+	static task task2;
+	task *tasks[] = {&task1, &task2};
+	unsigned char numTasks = sizeof(tasks)/sizeof(task*);
 	
-	_avr_timer_cntcurr = _avr_timer_M; // TimerISR will be called every _avr_timer_cntcurr 		milliseconds
+	//task 1
+	task1.state = keyPadOutput;
+	task1.period = systemPeriod;
+	task1.elapsedTime = task1.period;
+	task1.TickFct = &KeyPadTick;
 	
-	//enable global interrupts
-	SREG |= 0x80; //0x90: 10000000
+	//task 2
+	task2.state = OUTPUT;
+	task2.period = systemPeriod;
+	task2.elapsedTime = task2.period;
+	task2.TickFct = &LCDTick;
+	
+	TimerSet(systemPeriod);
+	TimerOn();
+	LCD_init();
+	LCD_ClearScreen();
+   unsigned char i;
+   while(1) {
+	   // Scheduler code
+	   for ( i = 0; i < numTasks; i++ ) {
+		   // Task is ready to tick
+		   if ( tasks[i]->elapsedTime >= tasks[i]->period ) {
+			   // Setting next state for task
+			   tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+			   // Reset the elapsed time for next tick.
+			   tasks[i]->elapsedTime = 0;
+		   }
+		   tasks[i]->elapsedTime += systemPeriod;
+	   }
+	   while(!TimerFlag);
+	   TimerFlag = 0;
+   }
+	return 0;
 }
 
-void TimerOff() {
-	TCCR1B = 0x00;
-}
-
-void TimerISR() {
-	unsigned char i;
-	for (i = 0; i < tasksNum; ++i) {
-		if (tasks[i].elapsedTime >= tasks[i].period) {
-			tasks[i].state = 				tasks[i].TickFnct(tasks[i].state);
-			tasks[i].elapsedTime = 0;
-		}
-		tasks[i].elapsedTime += taskPeriodGCD;
-	}
-}
-
-ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0) {
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-
-enum blink_state {bl_init, on};
-int BlinkLed(int state);
-
-enum three_state {init, on1, on2, on3};
-int ThreeLeds(int state);
-
-int BlinkLed(int state) {
-	switch(state) {
-		case bl_init:
-			state = on;
-			break;
-		case on:
-			state = bl_init;
-			break;
-		default:
-			state = bl_init;
-			break;
-	}
-	switch(state) {
-		case bl_init:
-			B3 = 0x00;
-			break;
-		case on:
-			B3 = 0x08;
-			break;
-		default:
-			break;
-	}
-	return state;		
-}
-
-int ThreeLeds(int state) {
-	switch(state) {
-		case init:
-			state = on1;
-			break;
-		case on1:
-			state = on2;
-			break;
-		case on2:
-			state = on3:
-			break;
-		case on3:
-			state = on1;
-			break;
-		default:
-			state = init;
-			break;
-	}
-	switch(state) {
-		case init:
-			break;
-		case on1:
-			B = 0x01;
-			break;
-		case on2:
-			B = 0x02;
-			break;
-		case on3:
-			B = 0x04;
-			break;
-		default:
+int KeyPadTick(int state){
+	switch(KeyPadState){
+		case keyPadOutput:
+			c = GetKeypadKey();
+			state = keyPadOutput;
 			break;
 	}
 	return state;
 }
 
-void Combine() {
-	PORTB = B | B3;
-}
-
-int main(void) {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	//DDRD = 0xFF; PORTD = 0x00;
-	
-	unsigned char i = 0;
-	tasks[i].state = bl_init;
-	tasks[i].period = periodbl;
-	tasks[i].elapsedTime = tasks[i].period;
-	tasks[i].TickFnct = &BlinkLed;
-	++i;	
-	tasks[i].state = init;
-	tasks[i].period = periodtl;
-	tasks[i].elapsedTime = tasks[i].period;
-	tasks[i].TickFnct = &ThreeLeds;
-	TimerSet(taskPeriodGCD);
-	TimerOn();
-
-	while (1) {
-		Combine();
-		}
-
-	return 1;
+int LCDTick(int state){
+	static unsigned char temp = ' ';
+	switch(state){
+		case OUTPUT:
+			if (c == '\0'){
+				LCD_Cursor(1);
+				LCD_WriteData(temp);
+			}
+			else {
+				temp = c;
+				LCD_Cursor(1);
+				LCD_WriteData(temp);
+			}
+			state = OUTPUT;
+			break;
+		default:
+			state = OUTPUT;
+			break;
+	}
+	return state;
 }
 
 
